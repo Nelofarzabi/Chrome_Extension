024bef846611b2e28532249987380e550be45dae
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-05-11 09:37:30 -0700

    Use template litural

--- script.js
+++ script.js
@@ -14,12 +14,19 @@ inputBtn.addEventListener('click' , function(){
 
 
 
-function renderLeads(){
-    let listItems = "";
-    for(let i = 0; i < myLeads.length ; i++){
-    listItems += "<li>" + "<a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] +"</a>" + "</li>";
-    
-         }
-    ulEl.innerHTML = listItems;
-}
+    function renderLeads(){
+        let listItems = "";
+        for(let i = 0; i < myLeads.length ; i++){
+        // listItems += "<li>" + "<a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] +"</a>" + "</li>";
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+                </a>
+            </li>
+        `
+        
+            }
+        ulEl.innerHTML = listItems;
+    }
 
