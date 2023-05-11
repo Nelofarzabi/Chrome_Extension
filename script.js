3e553158c9d1ad40214a055a7c3173d62ddc6b92
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-05-11 07:41:17 -0700

    Save every item

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ let ulEl = document.getElementById('ul-El');
 
 inputBtn.addEventListener('click' , function(){
         myLeads.push(inputEl.value);
+        inputEl.value=" ";
         renderLeads();
+       
+        
         
 })
+
 
 
 function renderLeads(){
     let listItems = "";
     for(let i = 0; i < myLeads.length ; i++){
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += "<li>" + "<a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] +"</a>" + "</li>";
+    
          }
     ulEl.innerHTML = listItems;
 }
