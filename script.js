0ddffcf5e1b86bc15405e3d464fa0003b6f32c2b
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-05-11 23:15:23 -0700

    Add mainfest file

--- script.js
+++ script.js
@@ -7,13 +7,7 @@ inputBtn.addEventListener('click' , function(){
         myLeads.push(inputEl.value);
         inputEl.value=" ";
         renderLeads();
-       
-        
-        
 })
-
-
-
     function renderLeads(){
         let listItems = "";
         for(let i = 0; i < myLeads.length ; i++){
